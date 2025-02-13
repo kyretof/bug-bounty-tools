4e05ca1c932b4b33e6c04aca0e3cb2e4221b0119
kyretof <kyretof@protonmail.com>
2025-02-14 05:53:39 +0800

    added open_redirects queries dict

--- google-dork-cli/script.py
+++ google-dork-cli/script.py
@@ -7,7 +7,8 @@
 '''
 
 queries = {
-    'open_redirects':'inurl:"next=" | inurl:"redirect=" | inurl:"url=" | inurl:"uri=" | inurl:"return=" -inurl:next- -inurl:redirect- -inurl:url- -inurl:uri- -inurl:return-'
+    'open_redirects': '(inurl:"redir=" OR inurl:"redirect=" OR inurl:"redirecturi=" OR inurl:"redirect_uri=" OR inurl:"redirecturl=" OR inurl:"return=" OR inurl:"returnurl=" OR inurl:"relaystate=" OR inurl:"forward=" OR inurl:"forwardurl=" OR inurl:"forward_url=" OR inurl:"url=" OR inurl:"uri=" OR inurl:"dest=" OR inurl:"destination=" OR inurl:"next=" OR inurl:"n=" OR inurl:"to=" OR inurl:"go=" OR inurl:"out=") -inurl:"n-" -inurl:"to-" -inurl:"go-" -inurl:"out-" -inurl:"uri-"
 }
 
-print(queries["open_redirects"])
+print(queries['open_redirects'])
+
